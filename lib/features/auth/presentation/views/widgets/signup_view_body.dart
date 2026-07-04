3bcfb1a6721aa0f093b9a5ecd6f660fbded420e5
Nady Alwazah <nadysy.7@gmail.com>
2026-07-05 01:43:00 +0300

    feat(auth): add SignupFormFields and integrate into SignupViewBody

--- lib/features/auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body.dart
@@ -1,20 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_task09_team_clean_architecture_app_beg/features/auth/presentation/views/widgets/auth_header.dart';
+import 'package:flutter_task09_team_clean_architecture_app_beg/features/auth/presentation/views/widgets/signup_form_fields.dart';
 
-class SignupViewBody extends StatelessWidget {
+class SignupViewBody extends StatefulWidget {
   const SignupViewBody({super.key});
 
   @override
+  State<SignupViewBody> createState() => _SignupViewBodyState();
+}
+
+class _SignupViewBodyState extends State<SignupViewBody> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  TextEditingController fullNameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    fullNameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
 
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            AuthHeader(title: "Register Account"),
-            SizedBox(height: 30),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              const AuthHeader(title: "Register Account"),
+              const SizedBox(height: 30),
+
+              SignupFormFields(
+                fullNameController: fullNameController,
+                emailController: emailController,
+                passwordController: passwordController,
+              ),
+            ],
+          ),
         ),
       ),
     );
