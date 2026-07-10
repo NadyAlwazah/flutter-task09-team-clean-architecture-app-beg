bcc3ba06c7fd50bd4c129eccf757f429eb4c2268
Nady Alwazah <nadysy.7@gmail.com>
2026-07-11 02:47:34 +0300

    Add onboarding, favourite, and notification

--- lib/features/notifications/presentation/views/notifications_view.dart
+++ lib/features/notifications/presentation/views/notifications_view.dart
@@ -1,11 +1,16 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_task09_team_clean_architecture_app_beg/features/notifications/presentation/views/widgets/notifications_view_body.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_task09_team_clean_architecture_app_beg/features/notifications/managers/notifications_cubit.dart';
+import 'package:flutter_task09_team_clean_architecture_app_beg/features/notifications/presentation/views/notifications-view_body.dart';
 
 class NotificationsView extends StatelessWidget {
   const NotificationsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: NotificationsViewBody());
+    return BlocProvider(
+      create: (context) => NotificationsCubit()..fetchNotifications(),
+      child: const NotificationsBody(),
+    );
   }
 }
